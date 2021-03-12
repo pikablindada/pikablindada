29176d2b15c538495b5b841ef0dc0384c1637b83
pikablindada <xbadoravel@gmail.com>
2021-03-12 12:53:57 -0300

    First commit

---
+++ src/menu.js
@@ -0,0 +1,40 @@
+const zeusmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do CAFFEZINMDZz:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  Feito por *caffezinmdzz*
+  DUVIDAS? 👇
+  WA.me/556198302738
+╚════════════════════`
+}
+
+ exports.menu = menu
+
+
+
+
+
+
+
+
